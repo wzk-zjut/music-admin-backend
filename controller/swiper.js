1fdd9aa82d126f05f243164a5a5cf531773f263a
wzk <zjutwzk@163.com>
2020-05-15 22:54:40 +0800

    wzk

--- controller/swiper.js
+++ controller/swiper.js
@@ -1,11 +1,68 @@
 const Router = require('koa-router')
 const router = new Router()
-const callCLoudDB = require('../utils/callCloudDB')
+const callCloudDB = require('../utils/callCloudDB')
+const cloudStorage = require('../utils/callCloudStorage')
 
 router.get('/getList', async (ctx, next) => {
     // 默认10条
     const query = `db.collection('swiper').get()`
-    const res = await callCLoudDB(ctx, 'databasequery', query)
-    console.log(res)
+    const res = await callCloudDB(ctx, 'databasequery', query)
+    // console.log(res)
+    let fileList = []
+    const data = res.data
+    for (let i = 0; i < data.length; i++) {
+        fileList.push({
+            fileid: JSON.parse(data[i]).fileid,
+            max_age: 7200
+        })
+    }
+    const dlRes = await cloudStorage.download(ctx, fileList)
+    // console.log(dlRes)
+    let returnData = []
+    for (let i = 0; i < dlRes.file_list.length; i++) {
+        returnData.push({
+            download_url: dlRes.file_list[i].download_url,
+            fileid: dlRes.file_list[i].fileid,
+            _id: JSON.parse(data[i])._id
+        })
+    }
+    ctx.body = {
+        code: 20000,
+        data: returnData
+    }
+})
+router.post('/upload', async (ctx, next) => {
+    const fileid = await cloudStorage.upload(ctx)
+    // console.log(fileid)
+    // 写数据库
+    const query = `
+         db.collection('swiper').add({
+             data: {
+                 fileid: '${fileid}'
+             }
+         })
+     `
+    const res = await callCloudDB(ctx, 'databaseadd', query)
+    ctx.body = {
+        code: 20000,
+        id_list: res.id_list
+    }
+})
+
+router.get('/del', async (ctx, next) => {
+    const params = ctx.request.query
+    // 删除云数据库中的内容
+    const query = `db.collection('swiper').doc('${params._id}').remove()`
+    const delDBRes = await callCloudDB(ctx, 'databasedelete', query)
+
+    // 删除云存储中的文件
+    const delStorageRes = await cloudStorage.delete(ctx, [params.fileid])
+    ctx.body = {
+        code: 20000,
+        data: {
+            delDBRes,
+            delStorageRes,
+        }
+    }
 })
 module.exports = router
